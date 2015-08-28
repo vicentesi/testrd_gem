6b01671dfbf52edb637d05fd151d397b74e018fe
Vicente Silveira Inácio <silveira.vicente@gmail.com>
2015-08-28 19:03:08 -0300

    Adding more tests

--- spec/testrd_gem_spec.rb
+++ spec/testrd_gem_spec.rb
@@ -1,15 +1,50 @@
-require 'spec_helper'
+require_relative 'spec_helper'
 
 describe TestrdGem do
-  it 'has a version number' do
-    expect(TestrdGem::VERSION).not_to be nil
-  end
 
-#  it 'does something useful' do
-#    expect(false).to eq(true)
-#  end
+  describe 'Lead' do
+    it 'has a version number' do
+      expect(TestrdGem::VERSION).not_to be nil
+    end
 
-  it 'has a lead' do
-    expect(TestrdGem::Lead.new("","","","","","","")).not_to be nil
+    it 'has empty array of leads' do
+      expect(TestrdGem::Lead.all).to be_empty
+    end
+
+    it 'has no leads' do
+      expect(TestrdGem::Lead.count).to be 0
+    end
+
+    it 'deletes inexistent lead' do
+      expect(TestrdGem::Lead.delete(0)).to be nil
+    end
+
+    it 'has a lead' do
+      expect(TestrdGem::Lead.count).to be 0
+      TestrdGem::Lead.new("","","","","","","")
+      expect(TestrdGem::Lead.count).to be 1
+    end
+
+    it 'returns a lead' do
+      expect(TestrdGem::Lead.new("","","","","","","")).not_to be nil
+    end
+
+    it 'descrease the lead counter' do
+      TestrdGem::Lead.new("","","","","","","")
+      old_count = TestrdGem::Lead.count
+      TestrdGem::Lead.delete(0)
+      new_count = TestrdGem::Lead.count
+
+      expect(new_count.to_i).to be < old_count.to_i
+    end
+
+    it 'return a set of leads' do
+      expect(TestrdGem::Lead.all).not_to be_empty
+    end
+
+    it 'should return exception (invalid login)' do
+      expect{TestrdGem::Lead.integrate("","","")}.to raise_error(RuntimeError)
+    end
+
   end
 end
