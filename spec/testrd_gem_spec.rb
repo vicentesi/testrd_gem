a04b0278fa763b70669f731ec3d1f7cf19ebac4e
Vicente Silveira Inácio <silveira.vicente@gmail.com>
2015-09-02 17:17:49 -0300

    Changing old tests to point to correct class.

--- spec/testrd_gem_spec.rb
+++ spec/testrd_gem_spec.rb
@@ -2,48 +2,48 @@ require_relative 'spec_helper'
 
 describe TestrdGem do
 
-  describe 'Lead' do
+  describe 'LeadStorage' do
     it 'has a version number' do
       expect(TestrdGem::VERSION).not_to be nil
     end
 
     it 'has empty array of leads' do
-      expect(TestrdGem::Lead.all).to be_empty
+      expect(TestrdGem::LeadStorage.all).to be_empty
     end
 
     it 'has no leads' do
-      expect(TestrdGem::Lead.count).to be 0
+      expect(TestrdGem::LeadStorage.count).to be 0
     end
 
     it 'deletes inexistent lead' do
-      expect(TestrdGem::Lead.delete(0)).to be nil
+      expect(TestrdGem::LeadStorage.delete(0)).to be nil
     end
 
     it 'has a lead' do
-      expect(TestrdGem::Lead.count).to be 0
-      TestrdGem::Lead.new("","","","","","","")
-      expect(TestrdGem::Lead.count).to be 1
+      expect(TestrdGem::LeadStorage.count).to be 0
+      TestrdGem::LeadStorage.new("","","","","","","")
+      expect(TestrdGem::LeadStorage.count).to be 1
     end
 
     it 'returns a lead' do
-      expect(TestrdGem::Lead.new("","","","","","","")).not_to be nil
+      expect(TestrdGem::LeadStorage.new("","","","","","","")).not_to be nil
     end
 
     it 'descrease the lead counter' do
-      TestrdGem::Lead.new("","","","","","","")
-      old_count = TestrdGem::Lead.count
-      TestrdGem::Lead.delete(0)
-      new_count = TestrdGem::Lead.count
+      TestrdGem::LeadStorage.new("","","","","","","")
+      old_count = TestrdGem::LeadStorage.count
+      TestrdGem::LeadStorage.delete(0)
+      new_count = TestrdGem::LeadStorage.count
 
       expect(new_count.to_i).to be < old_count.to_i
     end
 
     it 'return a set of leads' do
-      expect(TestrdGem::Lead.all).not_to be_empty
+      expect(TestrdGem::LeadStorage.all).not_to be_empty
     end
 
     it 'should return exception (invalid login)' do
-      expect{TestrdGem::Lead.integrate("","","")}.to raise_error(RuntimeError)
+      expect{TestrdGem::LeadStorage.integrate("","","")}.to raise_error(RuntimeError)
     end
 
   end
